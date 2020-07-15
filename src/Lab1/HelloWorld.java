318caad4d1f427493e989c0ac491071cc392ee10
supapun <supapun.s@rmutsvmail.com>
2020-07-15 14:55:50 +0700

    add more code

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -5,7 +5,10 @@ public class HelloWorld
     public static void main(String[] args) {
 
         System.out.println("Hello World !!!");
-        System.out.println("Hello World !!!");
+        System.out.println("ชื่อ นางสาวสุภาพันธ์ สายทองอินทร์");
+        System.out.println("รหัสนักศึกษา 363211760029");
+        System.out.println("สาขา เทคโนโลยีการจัดการ");
+        System.out.println("คณะ การจัดการเทคโนโลยีสารสนเทศ");
     }
 
 
