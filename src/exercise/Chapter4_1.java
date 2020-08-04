67ee32a6852a4798299cccf29b9890e6b6b1e571
supapun <supapun.s@rmutsvmail.com>
2020-08-04 23:01:47 +0700

    การบ้าน2

--- src/exercise/Chapter4_1.java
+++ src/exercise/Chapter4_1.java
@@ -1,4 +1,34 @@
 package exercise;
 
-public class 00Chapter4_1 {
+import java.util.Scanner;
+
+public class Chapter4_1 {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner (System.in);
+
+
+        System.out.print("Enter a score 1: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter a score 2: ");
+        int b = scanner.nextInt();
+        System.out.print("Enter a score 3: ");
+        int c = scanner.nextInt();
+        System.out.print("Enter a score 4: ");
+        int d = scanner.nextInt();
+        System.out.print("Enter a score 5: ");
+        int e = scanner.nextInt();
+        System.out.print("Enter a score 6: ");
+        int f = scanner.nextInt();
+        System.out.print("Enter a score 7: ");
+        int g = scanner.nextInt();
+        System.out.print("Enter a score 8: ");
+        int h = scanner.nextInt();
+        System.out.print("Enter a score 9: ");
+        int i = scanner.nextInt();
+        System.out.print("Enter a score 10: ");
+        int j = scanner.nextInt();
+
+
+        }
 }
